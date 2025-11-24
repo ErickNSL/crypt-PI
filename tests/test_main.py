f3f69af3ca4d57f5e0d346868f0aef7fece89f1f
ErickNSL <erickalvarezsh@gmail.com>
2025-11-23 19:06:44 -0600

    Test Adjust

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,16 @@ from app.main import app
 
 client = TestClient(app)
 
+# Test the ingestion  (E, L, T steps)
+def test_ingestion_endpoint():
+    response = client.get("/ingest")
+    assert response.status_code == 200
+    data = response.json()
+    
+    assert data["status"] == "Ingested"
+
+    assert data["rows"] >= 3
+
 # Test the core analytics endpoint
 def test_analytics_endpoint_structure():
     response = client.get("/analytics/avg-price")
@@ -14,12 +24,3 @@ def test_analytics_endpoint_structure():
     assert "analytics" in data
     assert isinstance(data["analytics"], list)
     
-# Test the ingestion  (E, L, T steps)
-def test_ingestion_endpoint():
-    response = client.get("/ingest")
-    assert response.status_code == 200
-    data = response.json()
-    
-    assert data["status"] == "Ingested"
-
-    assert data["rows"] >= 3
